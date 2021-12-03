2340883a99bf4505de89b1288de498031cddec7c
john-chown <johnchown@oakland.edu>
2021-12-03 17:45:50 -0500

    added all

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/resume", (req, res) => {
+    res.render("../views/html/my_resume");
+});
+
+app.get("/funfacts", (req, res) => {
+    res.render("../views/html/my_fun_facts");
+});
+
+app.get("/blog", (req, res) => {
+    res.render("../views/html/blog");
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`server is running on ${PORT}`));
