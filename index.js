63c4af42ec4780aff762bfb4b01809f195c14b7f
john-chown <johnchown@oakland.edu>
2021-12-04 21:02:37 -0500

    post method not working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,7 +9,6 @@ app.use(express.urlencoded({ extended: true}));
 
 app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
-
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -23,8 +23,41 @@ app.get("/funfacts", (req, res) => {
 });
 
 app.get("/trivia", (req, res) => {
+    
+    
+    
     res.render("../views/html/trivia");
 });
+
+app.post('/trivia', (req, res) => {
+
+    var score1 = 0;
+    var score2 = 0;
+    
+    console.log(req.body.answer1 + req.body.answer2);
+    
+        // if (req.body.answer1 == "a"){
+            
+        //     score1+= 1;
+           
+            
+            
+        // }
+        
+        //  if (req.body.answer2 == "d"){
+            
+        //     score2+= 1;
+            
+            
+        // }
+        
+    // sum = (score1 + score2)
+
+    // console.log(sum);
+   
+    
+})
+
 
 
 const PORT = process.env.PORT || 3000;
